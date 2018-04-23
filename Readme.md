9069368e714f71a13f25cb8939f0733b57fd2883
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# Customize data updates using the RowUpdating event
+
+
+<p>This example demonstrates how you can implement custom updates using the <strong>RowUpdating</strong> event.</p>
+
+<br/>
+
+
---
+++ VB/WebSite/Default.aspx.vb
@@ -0,0 +1,36 @@
+Imports Microsoft.VisualBasic
+Imports System
+Imports System.Data
+Imports System.Configuration
+Imports System.Collections
+Imports System.Web
+Imports System.Web.Security
+Imports System.Web.UI
+Imports System.Web.UI.WebControls
+Imports System.Web.UI.WebControls.WebParts
+Imports System.Web.UI.HtmlControls
+Imports DevExpress.Web.ASPxGridView
+
+Partial Public Class Grid_Editing_CustomUpdate_CustomUpdate
+	Inherits System.Web.UI.Page
+	Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
+		'Populate grid with data on the first load
+		If (Not IsPostBack) AndAlso (Not IsCallback) Then
+			Dim provider As InvoiceItemsProvider = New InvoiceItemsProvider()
+			provider.Populate()
+		End If
+	End Sub
+	Protected Sub grid_RowUpdating(ByVal sender As Object, ByVal e As DevExpress.Web.Data.ASPxDataUpdatingEventArgs)
+		Dim provider As InvoiceItemsProvider = New InvoiceItemsProvider()
+		Dim item As InvoiceItem = provider.GetItemById(CInt(Fix(e.Keys(0))))
+		If Not item Is Nothing Then
+			item.Name = Convert.ToString(e.NewValues("Name"))
+			item.Price = Convert.ToDecimal(e.NewValues("Price"))
+			item.Quantity = Convert.ToInt32(e.NewValues("Quantity"))
+		End If
+		'Cancel automatic update
+		e.Cancel = True
+		'back to the browse mode
+		CType(sender, ASPxGridView).CancelEdit()
+	End Sub
+End Class
